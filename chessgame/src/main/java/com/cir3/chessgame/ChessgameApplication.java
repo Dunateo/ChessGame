5c94824d6bee5473310967c46cd3e8642e1854e2
Valentin <perso@valentinbru.com>
2020-05-03 15:42:45 +0200

    pom.xml change

---
+++ chessgame/src/main/java/com/cir3/chessgame/ChessgameApplication.java
@@ -0,0 +1,13 @@
+package com.cir3.chessgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessgameApplication.class, args);
+	}
+
+}
---
+++ chessgame/src/test/java/com/cir3/chessgame/ChessgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cir3.chessgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
