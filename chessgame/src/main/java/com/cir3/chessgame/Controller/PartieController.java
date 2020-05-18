54c1811e041f9f6cd4a1f440ee465bfe793d7c27
JejeB <jeremy.bonnefoi@isen.yncrea.fr>
2020-05-18 15:20:51 +0200

    creation du template

--- chessgame/src/main/java/com/cir3/chessgame/Controller/PartieController.java
+++ chessgame/src/main/java/com/cir3/chessgame/Controller/PartieController.java
@@ -1,5 +1,15 @@
 package com.cir3.chessgame.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/partie")
 public class PartieController {
+	@GetMapping("=id")
+	public String partie() {
+		return "partie";
+	}
 
 }
