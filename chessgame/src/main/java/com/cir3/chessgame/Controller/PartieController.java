3f7b132e13f06abb5cf89714f7f0ef36dfb692b5
JejeB <jeremy.bonnefoi@isen.yncrea.fr>
2020-06-02 10:48:23 +0200

    controller pas Controller idiot

--- chessgame/src/main/java/com/cir3/chessgame/Controller/PartieController.java
+++ chessgame/src/main/java/com/cir3/chessgame/Controller/PartieController.java
@@ -1,4 +1,4 @@
-package com.cir3.chessgame.Controller;
+package com.cir3.chessgame.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
--- chessgame/src/main/java/com/cir3/chessgame/Controller/api/PartieApiController.java
+++ chessgame/src/main/java/com/cir3/chessgame/Controller/api/PartieApiController.java
@@ -1,4 +1,4 @@
-package com.cir3.chessgame.Controller.api;
+package com.cir3.chessgame.controller.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
