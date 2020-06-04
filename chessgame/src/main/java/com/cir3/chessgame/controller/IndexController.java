48223871410aa2d16e479cee60152ed337c2793f
Valentin <perso@valentinbru.com>
2020-06-05 01:08:52 +0200

    bug IndexController fixed

--- chessgame/src/main/java/com/cir3/chessgame/controller/IndexController.java
+++ chessgame/src/main/java/com/cir3/chessgame/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.cir3.chessgame.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,16 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-
     @GetMapping("/")
     public String welcome(Model model){
         return "index";
     }
 
     @GetMapping("/login")
-
     public String login(Authentication authentication) {
-
 
         //if authentified we send him on his user page
         if (authentication!= null && authentication.isAuthenticated()) {
