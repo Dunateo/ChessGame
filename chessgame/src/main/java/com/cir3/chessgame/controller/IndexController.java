d474d2e3fe8aec57328057134464e951b42f8b2f
JejeB <jeremy.bonnefoi@isen.yncrea.fr>
2020-06-05 01:31:26 +0200

    Nothing change

--- chessgame/src/main/java/com/cir3/chessgame/controller/IndexController.java
+++ chessgame/src/main/java/com/cir3/chessgame/controller/IndexController.java
@@ -3,20 +3,31 @@ package com.cir3.chessgame.controller;
 import com.cir3.chessgame.domain.Authority;
 import com.cir3.chessgame.domain.Joueur;
 import com.cir3.chessgame.repository.AuthorityRepository;
+import com.cir3.chessgame.repository.CouleurRepository;
 import com.cir3.chessgame.repository.JoueurRepository;
+import com.cir3.chessgame.repository.PionRepository;
+import com.cir3.chessgame.services.InitBddService;
+
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.ArrayList;
-import java.util.Optional;
+
 
 @Controller
 public class IndexController {
 
+	
+	
     //default controller
     @GetMapping("/")
     public String welcome(Model model){
@@ -36,5 +47,6 @@ public class IndexController {
         return "login";
     }
 
+    
 
 }
