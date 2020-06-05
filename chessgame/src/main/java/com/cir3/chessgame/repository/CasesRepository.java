8b4a8dc449c699b1e95cd3cec8bb8cc09384d06a
JejeB <jeremy.bonnefoi@isen.yncrea.fr>
2020-06-05 11:01:55 +0200

    Add correction

--- chessgame/src/main/java/com/cir3/chessgame/repository/CasesRepository.java
+++ chessgame/src/main/java/com/cir3/chessgame/repository/CasesRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CasesRepository extends JpaRepository<Cases,Long> {	
+public interface CasesRepository extends JpaRepository<Cases,Long> {
+	 Cases findByXAndYAndPartie_id(int x,int y,Long id);
 }
